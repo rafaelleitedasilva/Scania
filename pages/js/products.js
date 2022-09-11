c5271f48ca20ea5c54a646628a6c5e2a3b6453ea
evrocha <emanuelrocha831@gmail.com>
2022-09-11 16:32:00 -0300

    products-page

---
+++ pages/js/products.js
@@ -0,0 +1,49 @@
+let linha = document.getElementById("line-type")
+let desc = document.getElementById("desc_line")
+let link = document.getElementById("link")
+
+const changeLine1 = () =>{
+    linha.innerHTML = "Linha P"
+    desc.innerHTML = "A linha P da Scania é nossa linha de cabines mais versátil, ideal para operações urbanas e regionais e aprovada em construção e outras condições exigentes."
+
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/p-series.html")
+    // link.setAttribute("href", "google.com")
+    
+}
+
+const changeLine2 = () =>{
+    linha.innerHTML = "Linha G"
+    desc.innerHTML = "A linha G da Scania traz a infalível combinação de conforto e elegância por dentro e por fora. Oferecendo opções de armazenamento exclusivas, bem como uma cabine espaçosa."
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/g-series.html")
+}
+
+const changeLine3 = () =>{
+    linha.innerHTML = "Linha R"
+    desc.innerHTML = "Não há como negar – a elegante cabine está mais resistente e eficaz do que nunca. Prepare-se para virar cabeças com a carroceria atlética que redefine o conceito de 'premium' em caminhões de longa distância."
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/r-series.html")
+}
+
+const changeLine4 = () =>{
+    linha.innerHTML = "Linha S"
+    desc.innerHTML = "A linha S cria uma nova referência de conforto para o motorista de longa distância. Descubra um interior que é um paraíso de luxo, com um piso plano e instalações de armazenamento estendido, projetado para uma área espaçosa."
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/s-series.html")
+}
+
+const changeLine5 = () =>{
+    linha.innerHTML = "Linha V8"
+    desc.innerHTML = "O novo trem de força Scania V8 inicia uma incrível experiência de condução. Quando combinado com a nova caixa de câmbio Scania Opticruise, é possível obter uma economia de combustível de até 6%."
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/v8.html")
+}
+
+const changeLine6 = () =>{
+    linha.innerHTML = "Linha XT"
+    desc.innerHTML = "A linha XT da Scania foi criada para resistir a condições variadas e ambientes desafiadores, de modo que você realize uma operação lucrativa."
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/xt.html")
+}
+
+const changeLine7 = () =>{
+    linha.innerHTML = "Caminhões a gás"
+    desc.innerHTML = "A linha de veículos a gás da Scania está disponível nas versões CNG e LNG para tratores e aplicações rígidas. Com uma ampla seleção de configurações, você pode personalizar seu caminhão. "
+    link.setAttribute("href","https://www.scania.com/br/pt/home/products/trucks/gas-truck.html")
+}
+
