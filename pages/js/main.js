017f48f003ad88a74ea61ad9841dd76e5b6169a2
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-09-14 15:36:41 -0300

    Index -> Avaliações e Contato

--- pages/js/main.js
+++ pages/js/main.js
@@ -41,15 +41,25 @@
 
 
     // Testimonials carousel
-    $('.testimonial-carousel').owlCarousel({
+    $('.testimonial-slider').slick({
+        infinite: true,
         autoplay: true,
-        smartSpeed: 1000,
-        loop: true,
-        nav: false,
-        dots: true,
-        items: 1,
-        dotsData: true,
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial-slider-nav'
     });
+    $('.testimonial-slider-nav').slick({
+        arrows: false,
+        dots: false,
+        focusOnSelect: true,
+        centerMode: true,
+        centerPadding: '22px',
+        slidesToShow: 3,
+        asNavFor: '.testimonial-slider'
+    });
+    $('.testimonial .slider-nav').css({"position": "relative", "height": "160px"});
 
     
 })(jQuery);
