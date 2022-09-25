11b80464441a37eaf317622e0b2eb85db00717e8
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-09-25 17:08:35 -0300

    Desafio 3

--- pages/js/orcamento.js
+++ pages/js/orcamento.js
@@ -11,9 +11,10 @@ let imposto
 
 function formatReal( int ){
         var tmp = int+'';
-        tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
-        if( tmp.length > 6 )
+        tmp = tmp.replace(/([0-9]{2})$/g, "00,$1");
+        if(tmp.length > 6 ){
                 tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+        }
         return tmp;
 }
 
