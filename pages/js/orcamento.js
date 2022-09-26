b334ae37185eba996bc594f86bc12d93cf9e8350
evrocha <emanuelrocha831@gmail.com>
2022-09-26 10:03:35 -0300

    index-vid

--- pages/js/orcamento.js
+++ pages/js/orcamento.js
@@ -13,18 +13,18 @@ let h1 = document.getElementById('h1')
 let moeda = `R$`
 let imposto
 
+function dados(){
+  form.style = "display:block;"
+  form1.style = "display:none;"
+  h1.innerHTML = "Dados Pessoais"
+}
+
 function orcamento(){
   form.style = "display:none;"
   form1.style = "display:block;"
   aside.style = "display:none;"
   form2.style = "display:none;"
   h1.innerHTML = "Orçamento"
-}   
-
-function dados(){
-  form.style = "display:block;"
-  form1.style = "display:none;"
-  h1.innerHTML = "Dados Pessoais"
 }
 
 function forma(){
@@ -40,73 +40,97 @@ function forma(){
     h1.innerHTML = "Forma de Pagamento"
   }
 }
+
+function formatReal( int ){
+  var tmp = int+'';
+  tmp = tmp.replace(/([0-9]{2})$/g, "00,$1");
+  if(tmp.length > 6 ){
+    tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+  }
+  return tmp;
+}
+function formatExit(int){
+  int = int.replace(',','')
+  int = int.replace('.','')
+  return int
+}
+function retira00(int){
+  int = int.replace(',00','')
+  int = int.replace('.','')
+  return int
+}
+function formataEntrada(){
+  entrada.value = parseFloat(formatExit(retira00(entrada.value))).toLocaleString('pt-br', {minimumFractionDigits: 2})
+}
+function formataValor(){
+  valor.value = parseFloat(formatExit(retira00(valor.value))).toLocaleString('pt-br', {minimumFractionDigits: 2})
+}
+function formataCPF(){
+  let CPF = document.getElementById('CPF')
+  CPF.value = CPF.value.replace(/[^\d]/g, "");
+  CPF.value = CPF.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+function formatatelefone(){
+  let tel = document.getElementById('tel')
+  tel.value = tel.value.replace(/[^\d]/g, "");
+  tel.value = tel.value.replace(/(\d{2})(\d{5})(\d{4})/, "$1 $2-$3");
+}
+
+function moedaf(){
+  let moeda1 = document.getElementsByClassName('moeda')
+  switch (String(pais.value)) {
+    case 'Brasil':
+    moeda1[1].innerHTML = 'R$'
+    moeda1[0].innerHTML = 'R$'
+    break;
+    case 'Alemanha':
+    moeda1[1].innerHTML = '€'
+    moeda1[0].innerHTML = '€'
+    break;  
+    case 'Inglaterra':
+    moeda1[1].innerHTML = '£'
+    moeda1[0].innerHTML = '£'
+    break;  
+    case 'EUA':
+    moeda1[1].innerHTML = '$'
+    moeda1[0].innerHTML = '$'
+    break;    
+    case 'Suécia':
+    moeda1[1].innerHTML = 'kr'
+    moeda1[0].innerHTML = 'kr'
+    break;
+  }
+}
+
+function encerrar(){
+  switch (String(pais.value)) {
+    case 'Brasil':
+    moeda = 'R$'
+    imposto = 0.25
+    break;
+    case 'Alemanha':
+    moeda = '€'
+    imposto = 0.21
+    break;  
+    case 'Inglaterra':
+    moeda = "£"
+    imposto = 0.35
+    break;  
+    case 'EUA':
+    moeda = "$"
+    imposto = 0.05
+    break;    
+    case 'Suécia':
+    moeda = "kr"
+    imposto = 0.02
+    break;
+  }
   
-  
-  
-  function formatReal( int ){
-    var tmp = int+'';
-    tmp = tmp.replace(/([0-9]{2})$/g, "00,$1");
-    if(tmp.length > 6 ){
-      tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
-    return tmp;
-  }
-  function formatExit(int){
-    int = int.replace(',','')
-    int = int.replace('.','')
-    return int
-  }
-  function retira00(int){
-    int = int.replace(',00','')
-    int = int.replace('.','')
-    return int
-  }
-  function formataEntrada(){
-    entrada.value = parseFloat(formatExit(retira00(entrada.value))).toLocaleString('pt-br', {minimumFractionDigits: 2})
-  }
-  function formataValor(){
-    valor.value = parseFloat(formatExit(retira00(valor.value))).toLocaleString('pt-br', {minimumFractionDigits: 2})
-  }
-  function formataCPF(){
-    let CPF = document.getElementById('CPF')
-    CPF.value = CPF.value.replace(/[^\d]/g, "");
-    CPF.value = CPF.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-  }
-  
-  function formatatelefone(){
-    let tel = document.getElementById('tel')
-    tel.value = tel.value.replace(/[^\d]/g, "");
-    tel.value = tel.value.replace(/(\d{2})(\d{5})(\d{4})/, "$1 $2-$3");
-  }
-  
-  function encerrar(){
-    switch (String(pais.value)) {
-      case 'Brasil':
-      moeda = 'R$'
-      imposto = 0.25
-      break;
-      case 'Alemanha':
-      moeda = '€'
-      imposto = 0.21
-      break;  
-      case 'Inglaterra':
-      moeda = "£"
-      imposto = 0.35
-      break;  
-      case 'EUA':
-      moeda = "$"
-      imposto = 0.05
-      break;    
-      case 'Suécia':
-      moeda = "kr"
-      imposto = 0.02
-      break;
-    }
-    
-    let resultado = parseFloat(retira00(valor.value)) + (parseFloat(retira00(valor.value)) * parseFloat(imposto)) - parseFloat(retira00(entrada.value))
-    texto.innerHTML = `Você escolheu o caminhão da linha ${linha.value}, pelo(a) ${pais.value} que possui ${imposto*100}% de imposto`
-    texto2.innerHTML = `A sua entrada é de ${moeda}${entrada.value} e o valor bruto do caminhão escolhido é de ${moeda}${valor.value}`
-    console.log(resultado)
-    texto3.innerHTML = `O orçamento foi calculado no valor de ${moeda}${resultado.toLocaleString('pt-br', {minimumFractionDigits: 2})}`
-    aside.style = "display:block;"
-  }
+  let resultado = parseFloat(retira00(valor.value)) + (parseFloat(retira00(valor.value)) * parseFloat(imposto)) - parseFloat(retira00(entrada.value))
+  texto.innerHTML = `Você escolheu o caminhão da linha ${linha.value}, pelo(a) ${pais.value} que possui ${imposto*100}% de imposto`
+  texto2.innerHTML = `A sua entrada é de ${moeda}${entrada.value} e o valor bruto do caminhão escolhido é de ${moeda}${valor.value}`
+  console.log(resultado)
+  texto3.innerHTML = `O orçamento foi calculado no valor de ${moeda}${resultado.toLocaleString('pt-br', {minimumFractionDigits: 2})}`
+  aside.style = "display:block;"
+}
