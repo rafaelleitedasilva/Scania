4e3d8b7ea6db3fdd3312fe1ab4d6a6a4b3acc6da
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-09-28 13:08:04 -0300

    about

--- pages/js/about.js
+++ pages/js/about.js
@@ -1,8 +1,26 @@
-var video = document.getElementById("video");
-function darplay(){
-    let img = document.getElementById('imagemIframe')
-    img.style = "display:none;"
-    let vid = document.getElementById('video')
-    vid.style = "display:block;"
-    vid.setAttribute('autoplay')
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
